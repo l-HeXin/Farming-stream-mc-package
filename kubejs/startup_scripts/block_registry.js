5689bf33120e0b39cabee4f15a58984a9f7f2423
l-HeXin <jinjihao113795@outlook.com>
2023-01-31 21:49:12 +0800

    test

---
+++ kubejs/startup_scripts/block_registry.js
@@ -0,0 +1,4 @@
+onEvent("block.registry",event =>{
+    event.create('space_block').displayName('空间方块')
+
+})
--- kubejs/startup_scripts/item_registry.js
+++ kubejs/startup_scripts/item_registry.js
@@ -1,9 +1,12 @@
 onEvent('item.registry', event => {
+	event.create('space_piece').displayName('空间碎片')
+
+
+
 	event.create('gotou_hitori_goods').displayName('后藤一里')
 	event.create('ijichi_nijika_goods').displayName('伊地知虹夏')
 	event.create('kita_ikuyo_goods').displayName('喜多郁代')
 	event.create('yamada_ryo_goods').displayName('山田凉')
 
-	event.create('tool').displayName('彩蛋').texture("minecraft:egg")
-
+	
 })
